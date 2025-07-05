747d781fd9c3e3a8bf6d74c897d1f1e59685692c
huy01040104 <tronghuy01040@gmail.com>
2025-07-05 11:57:35 +0700

    fix: email not attached when user is logged in and clicked forgot password

--- src/app/core/auth/pages/forgot-password/forgot-password.component.ts
+++ src/app/core/auth/pages/forgot-password/forgot-password.component.ts
@@ -10,6 +10,7 @@ import { ButtonModule } from 'primeng/button';
 
 import { LoadingService } from '../../../../shared/services/core/loading/loading.service';
 import { PasswordService } from '../../services/password.service';
+import { UserService } from '../../../../shared/services/api/user/user.service';
 
 import { AuthLayoutComponent } from '../../auth-layout/auth-layout.component';
 import { FormControlComponent } from '../../../../shared/components/form-control/form-control.component';
@@ -33,15 +34,17 @@ export class ForgotPasswordComponent {
   private readonly fb = inject(FormBuilder);
   private readonly loadingService = inject(LoadingService);
   private readonly passwordService = inject(PasswordService);
+  private readonly userService = inject(UserService);
 
   form: FormGroup;
 
   isLoading = this.loadingService.isLoading;
+  currentUser = this.userService.currentUser;
   submitted = signal<boolean>(false);
 
   constructor() {
     this.form = this.fb.group({
-      email: '',
+      email: this.currentUser() ? this.currentUser()?.email : '',
     });
   }
 
