cbae9bcf77d504f6d82ece0d5a942f37bbd501fe
Nguyen Quy <quynx9503.dev@gmail.com>
2025-07-20 17:38:44 +0700

    fix: Fix bug sonar scan

--- src/app/shared/services/api/lesson-materials/lesson-materials.service.ts
+++ src/app/shared/services/api/lesson-materials/lesson-materials.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 
-import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
+import { Observable, catchError, map, tap, throwError } from 'rxjs';
 
 import { environment } from '../../../../../environments/environment';
 import { RequestService } from '../../core/request/request.service';
